4652b2921d10b4666e727d40d2be1c73fb60b05d
“Eugeny <prochan_ev@itms.tech>
2023-03-19 08:46:05 +0300

    1 lesson

--- blink_star.py
+++ blink_star.py
@@ -1,27 +1,13 @@
 import time
 import curses
-import itertools
-import random
-
-
-def star():
-    frames = (
-        (('*', curses.A_DIM), lambda: time.sleep(2)),
-        (('*', ), lambda: time.sleep(0.3)),
-        (('*', curses.A_BOLD), lambda: time.sleep(0.5)),
-        (('*', ), lambda: time.sleep(0.3))
-    )
-    for frame_definition, pause_after in itertools.cycle(frames):
-        yield frame_definition, pause_after
 
 
 def draw(canvas):
     curses.curs_set(False)
     row, column = (curses.LINES // 2, curses.COLS // 2)
-    for frame, pause in star():
-        canvas.addstr(row, column, *frame)
-        canvas.refresh()
-        pause()
+    canvas.addstr(row, column, 'Hello world!')
+    canvas.refresh()
+    time.sleep(10)
 
 
 if __name__ == '__main__':
